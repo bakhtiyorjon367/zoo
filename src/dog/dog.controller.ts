3de71e230f701d857549f43c325f311ffecdc48a
bakhtiyorjon367 <bakhtee.0302@gmail.com>
2024-08-17 15:46:18 +0900

    feat: create post api request

--- src/dog/dog.controller.ts
+++ src/dog/dog.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Query, Req, Res } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query, Req, Res } from '@nestjs/common';
 import { DogService } from './dog.service';
 import {Request, Response} from 'express'
 
@@ -17,5 +17,12 @@ export class DogController {
      console.log("query", query);
        
       return this.dogService.introduce();
+    }
+
+    @Post('edit')
+    public modifyDetail(@Body() body:any): string {
+     console.log("body", body);
+       
+      return this.dogService.modifyDetail();
     }
 }
